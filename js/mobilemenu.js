b69e225b41d8ce738177190a8d64e9dd482672ff
Kamil Bała <komodres@gmail.com>
2017-01-22 00:41:25 +0100

    complete

---
+++ js/mobilemenu.js
@@ -0,0 +1,35 @@
+	 // DOM ready
+	 $(function() {
+	 	$('*[data-animate]').addClass('hide').each(function(){
+			$(this).viewportChecker({
+				classToAdd: 'show animated ' + $(this).data('animate'),
+				classToRemove: 'hide',
+				offset: 200
+			});
+		});
+	   
+      // Create the dropdown base
+      $("<select />").appendTo("nav");
+      
+      // Create default option "Go to..."
+      $("<option />", {
+         "selected": "selected",
+         "value"   : "",
+         "text"    : "Menu"
+      }).appendTo("nav select");
+      
+      // Populate dropdown with menu items
+      $("nav a").each(function() {
+       var el = $(this);
+       $("<option />", {
+           "value"   : el.attr("href"),
+           "text"    : el.text()
+       }).appendTo("nav select");
+      });
+      
+	   // To make dropdown actually work
+	   // To make more unobtrusive: http://css-tricks.com/4064-unobtrusive-page-changer/
+      $("nav select").change(function() {
+        window.location = $(this).find("option:selected").val();
+      });
+	 });
